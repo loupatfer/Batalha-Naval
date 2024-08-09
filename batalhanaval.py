e8ac0683358a58158b8a79c8d16c8b6ce61e19af
loupatfer <145603398+loupatfer@users.noreply.github.com>
2024-08-09 23:01:57 +0100

    Batalha Naval 1.0.0
    Versão inicial com testes ainda no terminal

--- batalhanaval.py
+++ batalhanaval.py
@@ -1,7 +1,7 @@
 import random
 
 nomes_barcos = ["Carrier", "Battleship", "Fragate", "Submarine", "Destroyer"]
-#Tamanho por ordem de nome: 5,4,3,3,2
+# Tamanho por ordem de nome: 5,4,3,3,2
 tamanho_barcos = {"Carrier": 5, "Battleship": 4, "Fragate": 3, "Submarine": 3, "Destroyer": 2}
 pontos_barcos = {"C": 100, "B": 80, "F": 60, "S": 60, "D": 40}
 
@@ -9,7 +9,7 @@ def obter_tamanho_barco(nome):
     return tamanho_barcos.get(nome, 0)
 
 def obter_pontos_barco(nome):
-    return pontos_barcos.get(nome,0)
+    return pontos_barcos.get(nome[0], 0)
 
 def nome_jogador():
     while True:
@@ -17,7 +17,7 @@ def nome_jogador():
         if username:
             print(f'Bem vindo {username}!')
             return username
-        
+
 tamanho_mapa = 10
 
 def criar_campo(tamanho_mapa):
@@ -28,8 +28,6 @@ def mostrar_campo(campo):
         print(" ".join(row))
 
 def coordenada_barco_utilizador(campo_utilizador, campo_ocupado, nome):
-    existe = 0
-    i = 0
     while True:
         try:
             row = int(input(f"Insere a linha para o {nome} (0-9): "))
@@ -96,12 +94,12 @@ def coloca_barco_aleatorio(campo, campo_ocupados, nome):
                     campo_ocupados[row][col + i] = "X"
                     campo[row][col + i] = nome[0]
             return
-        
+
 def atacar(campo_oponente, campo_visivel, row, col, pontuacao_jogador):
     if campo_visivel[row][col] != "_":
         print("Esta coordenada já foi atacada")
-        return False
-    
+        return False, pontuacao_jogador
+
     if campo_oponente[row][col] != "_":
         print("Acertou em um barco!")
         barco_acertado = campo_oponente[row][col]
@@ -109,20 +107,21 @@ def atacar(campo_oponente, campo_visivel, row, col, pontuacao_jogador):
         campo_oponente[row][col] = "X"
 
         if not any(barco_acertado == campo_oponente[r][c] for r in range(tamanho_mapa) for c in range(tamanho_mapa)):
-            for nome, inicial in zip(nome_barcos, [n[0] for n in nome_barcos]):
+            for nome, inicial in zip(nomes_barcos, [n[0] for n in nomes_barcos]):
                 if inicial == barco_acertado:
-                    pontuacao_jogador += obter_pontos_barco(nome)
-                    print(f"Destruíste um {nome}! Ganhou {obter_pontos_barco(nome)} pontos!")
+                    pontos = obter_pontos_barco(nome)
+                    pontuacao_jogador += pontos
+                    print(f"Destruíste um {nome}! Ganhou {pontos} pontos!")
                     break
     else:
         print("Errou")
         campo_visivel[row][col] = "-"
-    return True
+    return True, pontuacao_jogador
 
 def computador_ataca(campo_jogador, campo_visivel_computador, pontuacao_computador):
     while True:
         row = random.randint(0, tamanho_mapa - 1)
-        col = random.randint(0, tamanho_mapa -1)
+        col = random.randint(0, tamanho_mapa - 1)
 
         if campo_visivel_computador[row][col] != "_":
             continue
@@ -136,19 +135,21 @@ def computador_ataca(campo_jogador, campo_visivel_computador, pontuacao_computad
             campo_jogador[row][col] = "X"
 
             if not any(barco_acertado == campo_jogador[r][c] for r in range(tamanho_mapa) for c in range(tamanho_mapa)):
-                for nome, inicial in zip(nome_barcos, [n[0] for n in nome_barcos]):
+                for nome, inicial in zip(nomes_barcos, [n[0] for n in nomes_barcos]):
                     if inicial == barco_acertado:
-                        pontuacao_computador += obter_pontos_barco(nome)
-                        print(f"O Computador destruiu o teu {nome} e ganhou {obter_pontos_barco(nome)} pontos!")
+                        pontos = obter_pontos_barco(nome)
+                        pontuacao_computador += pontos
+                        print(f"O Computador destruiu o teu {nome} e ganhou {pontos} pontos!")
                         break
         else:
             print("Computador errou!")
             campo_visivel_computador[row][col] = "-"
-        break   
+        break
+    return pontuacao_computador
 
 if __name__ == "__main__":
     nome_jogador()
-    
+
     campo_utilizador = criar_campo(tamanho_mapa)
     campo_computador = criar_campo(tamanho_mapa)
     campo_ocupados_usuario = criar_campo(tamanho_mapa)
@@ -156,16 +157,16 @@ if __name__ == "__main__":
 
     pontuacao_jogador = 0
     pontuacao_computador = 0
-    
+
     for nome_barcos in nomes_barcos:
         coordenada_barco_utilizador(campo_utilizador, campo_ocupados_usuario, nome_barcos)
-    
+
     for nome_barcos in nomes_barcos:
         coloca_barco_aleatorio(campo_computador, campo_ocupados_computador, nome_barcos)
 
     print("\n\n****************CAMPOS USUARIO***********************\n\n")
     mostrar_campo(campo_utilizador)
-    
+
     print("\n\n\n****************CAMPOS COMPUTADOR**********************\n\n")
     mostrar_campo(campo_computador)
 
@@ -178,20 +179,20 @@ if __name__ == "__main__":
                 print("\nAtaca o tabuleiro do computador!")
                 row = int(input(f"Insere a linha para o ataque (0-9): "))
                 col = int(input(f"Insere a coluna para o ataque (0-9): "))
-                if atacar(campo_computador, campo_visivel_jogador, row, col,pontuacao_jogador):
+                ataque_valido, pontuacao_jogador = atacar(campo_computador, campo_visivel_jogador, row, col, pontuacao_jogador)
+                if ataque_valido:
                     break
             except ValueError:
                 print("Por favor, insere um inteiro válido.")
             except IndexError:
                 print("Coordenadas fora do limite do mapa. Tente novamente.")
-        
+
         print("\nTabuleiro do jogador após o ataque:\n")
         mostrar_campo(campo_visivel_jogador)
         print(f"A tua pontuação é de {pontuacao_jogador} pontos!")
 
-        computador_ataca(campo_utilizador, campo_visivel_computador, pontuacao_computador)
+        pontuacao_computador = computador_ataca(campo_utilizador, campo_visivel_computador, pontuacao_computador)
 
         print("\nTabuleiro do jogador após o ataque do computador: ")
-        mostrar_campo(campo_visivel_jogador)
+        mostrar_campo(campo_visivel_computador)
         print(f"Pontuação do computador: {pontuacao_computador} pontos")
-
